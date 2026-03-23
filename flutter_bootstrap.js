90c39dd1d488b7c30eda1d1a4c98dcd5be9b3a05
sanyahalacheva <sanya.halacheva@gmail.com>
2026-03-23 17:43:12 +0200

    fix app name

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2423317459"
+    serviceWorkerVersion: "906187591"
   }
 });
